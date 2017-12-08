886f89cd499fe0bd72051b59df388321ae14b7ba
jackarens <31498160+jackarens@users.noreply.github.com>
2017-12-07 21:20:51 -0600

    Update weather.c

--- weather.c
+++ weather.c
@@ -2,11 +2,14 @@
 #include<stdio.h>
 #include<string.h>
 
+//defines the WeatherData struct
 typedef struct{
 	char *date;
 	double temp;
 } WeatherData;
 
+//this function builds and returns an array of pointers to the WeatherData struct
+//simply creates all the open spaces needed to hold the data and fills them from file f
 WeatherData ** createDate(FILE *f, int *size) {
 	int i, errorCheck;
 	char *temp = (char *)malloc(sizeof(char) * 2);
@@ -23,19 +26,24 @@ WeatherData ** createDate(FILE *f, int *size) {
 	return outPut;
 }
 
+//comparator for stadiums (by date)
 int compareStationDates(const void *stationA, const void *stationB) {
-	WeatherData *dateA = *(WeatherData **)stationA;
+	WeatherData *dateA = *(WeatherData **)stationA; //funky casting and dereferencing stuff b/c we have an array of an array of pointers
 	WeatherData *dateB = *(WeatherData **)stationB;
 	//printf("%s, %s\n", dateA->date, dateB->date);
 	return strcmp(dateA->date, dateB->date);
 }
 
 int main(int argc, char **argv) {
-	if (argc != 3) {
+	
+	//ensures two file names are inputted
+	if (argc != 3) { 
 		printf("Error, please input 2 file names\n");
 		return 0;
 	}
-	FILE *f1 = fopen(argv[1], "r");
+	
+	//opens files
+	FILE *f1 = fopen(argv[1], "r"); 
 	FILE *f2 = fopen(argv[2], "r");
 	if (f1 == NULL || f2 == NULL) {
 		printf("Error in reading files\n");
@@ -44,10 +52,16 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 	int size1, size2, a = 0, b = 0;
-	WeatherData **stationData1 = createDate(f1, &size1);
+	
+	//creates two weatherdata structs (one for each station)
+	WeatherData **stationData1 = createDate(f1, &size1); 
 	WeatherData **stationData2 = createDate(f2, &size2);
+	
+	//sorts two weatherdata structs (one for each station)
 	qsort(stationData1, size1, sizeof(WeatherData *), compareStationDates);
 	qsort(stationData2, size2, sizeof(WeatherData *), compareStationDates);
+	
+	//first checks date. if correct, checks temps. if not, iterates the "behind" date by one and rechecks. prints result
 	while (a < size1 && b < size2) {
 		if (strcmp(stationData1[a]->date, stationData2[b]->date) == 0) {
 			if (stationData1[a]->temp != stationData2[b]->temp) {
@@ -65,6 +79,8 @@ int main(int argc, char **argv) {
 			b++;
 		}
 	}
+	
+	//closes file
 	fclose(f1);
 	fclose(f2);
 	return 0;
